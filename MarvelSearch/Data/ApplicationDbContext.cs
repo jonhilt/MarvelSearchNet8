4d2ca658a0bf255c2da2c7342f0db0972a0d85b4
Jon <jon@jonhilton.net>
2023-10-12 12:03:47 +0100

    Refactored search result to own component and implemented favourites

--- MarvelSearch/Data/ApplicationDbContext.cs
+++ MarvelSearch/Data/ApplicationDbContext.cs
@@ -1,8 +1,20 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace MarvelSearch.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
+    
+    public DbSet<Favourite> Favourite { get; set; }
+    
+}
+
+public class Favourite
+{
+    public int Id { get; set; }
+    public Guid UserId { get; set; }
+    public int MarvelId { get; set; }
+    public string CharacterName { get; set; }
 }
---
+++ MarvelSearch/Data/Migrations/20231012101616_Favourites.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarvelSearch.Migrations
+{
+    /// <inheritdoc />
+    public partial class Favourites : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Favourite",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UserId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    MarvelId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CharacterName = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Favourite", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Favourite");
+        }
+    }
+}
